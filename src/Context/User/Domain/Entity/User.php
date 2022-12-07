c9923f6ba249db9697c8067902e3a4054b826da1
Simtel <simuls@gmail.com>
2022-12-07 18:26:05 +0400

    add user

---
+++ src/Context/User/Domain/Entity/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Context\User\Domain\Entity;
+
+use App\Context\User\Infrastructure\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private string $email;
+
+    #[ORM\Column(type: 'string')]
+    private string $password;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
