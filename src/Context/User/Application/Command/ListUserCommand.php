8ecbfcdb15d52e12c67a9e71d328b35a4ddc765d
Simtel <simuls@gmail.com>
2023-02-08 10:50:18 +0400

    sql log in command

--- src/Context/User/Application/Command/ListUserCommand.php
+++ src/Context/User/Application/Command/ListUserCommand.php
@@ -6,7 +6,6 @@ namespace App\Context\User\Application\Command;
 
 use App\Context\Common\Infrastructure\Service\DoctrineConsoleLogger;
 use App\Context\User\Domain\Contract\UserRepositoryInterface;
-use App\Context\User\Domain\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -22,17 +21,19 @@ class ListUserCommand extends Command
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly UserRepositoryInterface $userRepository
+        private readonly UserRepositoryInterface $userRepository,
     ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->entityManager->getConnection()->getConfiguration()->setSQLLogger(new DoctrineConsoleLogger($output, true));
+        $this->entityManager->getConnection()->getConfiguration()->setSQLLogger(
+            new DoctrineConsoleLogger($output, true)
+        );
+
         $io = new SymfonyStyle($input, $output);
 
-        $this->entityManager->find(User::class, 3);
         $users = $this->userRepository->findAll();
 
         $headers = ['id', 'name', 'email'];
