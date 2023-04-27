935e82d217d9a18eef69ecadaca20a7a45c75754
Simtel <simuls@gmail.com>
2023-04-27 10:02:11 +0400

    get columns and types

---
+++ src/Context/Common/Application/Command/DoctrineGetTablesStructure.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Context\Common\Application\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand('example:doctrine-get-tables-structure')]
+class DoctrineGetTablesStructure extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $metadataFactory = $this->entityManager->getMetadataFactory();
+        $headers = ['field', 'type'];
+        foreach ($metadataFactory->getAllMetadata() as $metaData) {
+            $io->info($metaData->getName());
+
+            $rows = [];
+            foreach ($metaData->getColumnNames() as $columnName) {
+                $rows[] = [$columnName, $metaData->getTypeOfColumn($columnName)];
+            }
+            $io->table($headers, $rows);
+        }
+
+        return Command::SUCCESS;
+    }
+}
